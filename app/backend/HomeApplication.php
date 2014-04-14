888d3748b7e48f609cbdabf8c23b46c2095c1ec2
Arief M Utama <arief.utama@gmail.com>
2014-04-15 02:07:25 +0700

    DT test with database
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/backend/HomeApplication.php
+++ app/backend/HomeApplication.php
@@ -6,23 +6,34 @@ use PetakUmpet\Application;
 
 use PetakUmpet\UI\DataTables;
 
+use PetakUmpet\Database\Accessor;
+
 class HomeApplication extends Application {
 
   public function indexAction()
   {
     $dt = new DataTables($this->request);
     $dt->setDataSourceAction('Home/source');
-    $dt->setColumnNames(array('id', 'no', 'nama_orang', 'deskripsi'));
-    $dt->setColumnAlias(array('no' => 'Nomer'));
+    $dt->setColumnNames(array('id', 'userid', 'name'));
+    $dt->setColumnAlias(array('userid' => 'Id Pengguna', 'name' => 'Nama'));
     return $this->render(array('dt' => $dt));
   }
 
   public function sourceAction()
   {
+    // $a['aaData'] = array();
+    // for($i=1; $i<20; $i++) {
+    //   $row = array('id' => $i, 'no' => $i, 'nama_orang' => 'AMU-' . $i, 'deskripsi' => 'Sungguh bosan yah ' . (string) ($i*2-4+3));
+    //   $a['aaData'][] = $row;
+    // }
+
+    $dba = new Accessor('userdata');
+
+    $res = $dba->findAll();
     $a['aaData'] = array();
-    for($i=1; $i<20; $i++) {
-      $row = array('id' => $i, 'no' => $i, 'nama_orang' => 'AMU-' . $i, 'deskripsi' => 'Sungguh bosan yah ' . (string) ($i*2-4+3));
-      $a['aaData'][] = $row;
+
+    foreach ($res as $r) {
+      $a['aaData'][] = $r;
     }
 
     echo json_encode($a);
