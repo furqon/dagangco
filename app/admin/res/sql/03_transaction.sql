721abb03100f363467bdd78d10d8be2e390fe855
furqon <ahmad.furqon@gmail.com>
2014-05-07 01:27:00 +0700

    update transaction table structure

--- app/admin/res/sql/03_transaction.sql
+++ app/admin/res/sql/03_transaction.sql
@@ -1,6 +1,8 @@
 CREATE TABLE admin_transaction (
   id serial,
   warung_id integer,
+  customer_id integer,
+  status varchar(20), -- order/booking, shipping, payment, close
   name varchar(100),
   sales_price integer,
   shipment_method varchar(100),
@@ -9,6 +11,7 @@ CREATE TABLE admin_transaction (
   created_by integer,
   PRIMARY KEY (id),  
   FOREIGN KEY (warung_id) REFERENCES admin_warungdata (id),
+  FOREIGN KEY (customer_id) REFERENCES admin_customerdata (id),
   FOREIGN KEY (created_by) REFERENCES admin_userdata (id)
 );
 
